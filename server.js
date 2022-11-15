a50de0d2c180a5ccf5b9f92e682b002b4200d6d3
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-11-15 19:49:56 +0100

    Run yarn build in client files, change midleware in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db.js');
+const path = require('path');
+
 
 const app = express();
 
@@ -12,12 +14,19 @@ const seats = require('./routes/seats.routes.js');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 
 app.use('/api', testimonials);
 app.use('/api', concerts);
 app.use('/api', seats);
 
+// Serve static files from the React app
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 
 
@@ -26,6 +35,6 @@ app.use((req, res) => {
     res.status(404).send('Not found...');
   })
 
-app.listen(8000, () => {
+  app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
   });
